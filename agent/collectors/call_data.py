78a7e5883eb598159c00b62f93752b5a925b9d1d
salmanshariarmy <salmanshariardipu@gmail.com>
2026-07-31 04:28:29 +0800

    Update call_data.py

--- agent/collectors/call_data.py
+++ agent/collectors/call_data.py
@@ -1,1 +1,37 @@
+import subprocess
 
+
+def get_calls():
+
+    calls = []
+
+    try:
+
+        output = subprocess.check_output(
+            [
+                "content",
+                "query",
+                "--uri",
+                "content://call_log/calls",
+                "--projection",
+                "number:date:duration:type"
+            ],
+            text=True
+        )
+
+
+        for line in output.splitlines():
+
+            if line.startswith("Row"):
+
+                calls.append(line)
+
+
+    except Exception as e:
+
+        calls.append(
+            str(e)
+        )
+
+
+    return calls
